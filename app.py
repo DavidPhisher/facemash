b6e6cc44200ae0c5b7474bd79ab87dc0bc70649d
DavidPhisher <davidphisher8@gmail.com>
2024-11-04 23:57:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,40 +3,24 @@ import random
 
 app = Flask(__name__)
 
-# Example dataset of images
+# Sample images and initial votes
 images = [
-    ["img1", "static/images/1.jpg", 0],
-    ["img2", "static/images/2.jpg", 0],
-    ["img2", "static/images/3.jpg", 0],
-    ["img2", "static/images/4.jpg", 0],
-    ["img2", "static/images/5.jpg", 0],
-    ["img2", "static/images/6.jpg", 0],
-    ["img2", "static/images/7.jpg", 0],
-    ["img2", "static/images/8.jpg", 0],
-    ["img2", "static/images/9.jpg", 0],
-    ["img2", "static/images/10.jpg", 0]
-    # Add more images as needed
+    {"url": "static/image1.jpg", "votes": 0},
+    {"url": "static/image2.jpg", "votes": 0},
 ]
-
 
 @app.route('/')
 def index():
     return render_template('index.html', images=images)
 
-
 @app.route('/vote', methods=['POST'])
 def vote():
-    data = request.get_json()
-    winner_id = data['winner_id']
-
-    # Update vote count logic (here, just incrementing for the winner)
-    for image in images:
-        if image[0] == winner_id:
-            image[2] += 1
-
-    # Return updated image data
+    voted_image = request.json.get('image')
+    for img in images:
+        if img['url'] == voted_image:
+            img['votes'] += 1
+            break
     return jsonify(images=images)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
