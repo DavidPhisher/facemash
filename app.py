4e5610ebc91b681559862f4b2262f80c2d19e0b7
DavidPhisher <davidphisher8@gmail.com>
2024-11-05 00:00:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template, request, jsonify
+import os
 import random
 
 app = Flask(__name__)
 
-# Sample images and initial votes
-images = [
-    {"url": "static/image1.jpg", "votes": 0},
-    {"url": "static/image2.jpg", "votes": 0},
-]
+# Path to the images directory
+images_dir = 'static/images/'
+# List of images
+image_files = [f for f in os.listdir(images_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
+# Initialize votes for images
+votes = {img: 0 for img in image_files}
 
 @app.route('/')
 def index():
-    return render_template('index.html', images=images)
+    # Select two random images
+    selected_images = random.sample(image_files, 2)
+    return render_template('index.html', images=selected_images)
 
 @app.route('/vote', methods=['POST'])
 def vote():
     voted_image = request.json.get('image')
-    for img in images:
-        if img['url'] == voted_image:
-            img['votes'] += 1
-            break
-    return jsonify(images=images)
+    if voted_image in votes:
+        votes[voted_image] += 1
+    return jsonify(votes=votes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
